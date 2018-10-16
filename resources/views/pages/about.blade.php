7a110c95aa4ccb86ae28d3e7634f69c6fabbf48e
shri <sm2689@njit.edu>
2018-10-15 20:47:22 -0400

    Task:added Style to About page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.default')
 @section('content')
-    <h1>Welcome to My About Page</h1>
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+        <div class="container pt-sm-5 pb-5">
+            <h1>This is My About Page</h1>
+        </div>
 @endsection
