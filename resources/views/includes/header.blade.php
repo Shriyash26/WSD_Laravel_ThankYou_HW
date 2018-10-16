a98c1548b252d0bb129eb2893607b6249d793d8d
shri <sm2689@njit.edu>
2018-10-15 21:35:07 -0400

    Task:added Contact to Nav bar

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -15,6 +15,9 @@
                 <li class="nav-item ">
                     <a class="nav-link " href="/about">About</a>
                 </li>
+                <li class="nav-item ">
+                    <a class="nav-link " href="/contact">Contact</a>
+                </li>
 
             </ul>
 
