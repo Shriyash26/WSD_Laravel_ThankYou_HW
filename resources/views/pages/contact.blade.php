dfae475c9de42a05b9a6c309ce6abc7f86acb98b
shri <sm2689@njit.edu>
2018-10-17 13:26:25 -0400

    fix:added Name filed in contact form

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -4,11 +4,22 @@
         <div class="card-title ml-4 mt-3"><h1>Contact</h1>
             <p>Use this form to contact site owner</p></div>
         <div class="card-body">
-            <form action="/contact" method="post">
+            <form action="/contact" method="POST" action="{{route('contact.store')}}">
                 {{csrf_field()}}
-                <div class="form-group">
-                    <label for="Email1_Id">Email</label>
-                    <input name="Email" type="Email" class="form-control" id="Email" placeholder="name@example.com">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="Email1_Id">Email</label>
+                            <input name="Email" type="email" class="form-control" id="Email"
+                                   placeholder="name@example.com">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input name="name" type="text" class="form-control" id="name">
+                        </div>
+                    </div>
                 </div>
                 <div class="form-group">
                     <label for="Message">Message</label>
